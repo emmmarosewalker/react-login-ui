2f32d03a93271544d802de6647422a34fae63c3c
Emma Walker <emmmarosewalker@gmail.com>
2018-04-19 21:03:15 +1000

    removed unused import statements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Card } from './Card.js';
-import { Button } from './Button.js';
-import { Input } from './Input.js';
 import './App.css';
 
 class App extends Component {
